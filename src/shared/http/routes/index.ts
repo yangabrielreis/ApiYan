5eff7af1c178908fe21016806c97ecf5ec8cd17d
yangabrielreis <yangabrielreis@gmail.com>
2025-08-19 23:42:00 -0300

    aula 9 terminada

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -3,6 +3,7 @@ import albumsRouter from "@modules/albums/routes/albums.routes";
 import usersRouter from "@modules/users/routes/users.routes";
 import sessionsRouter from "@modules/users/routes/sessions.routes";
 import passwordRouter from "@modules/users/routes/password.routes";
+import profileRouter from "@modules/users/routes/profile.routes";
 
 const routes = Router();
 routes.get('/', (request, response) =>{
@@ -14,5 +15,6 @@ routes.use('/albums', albumsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
